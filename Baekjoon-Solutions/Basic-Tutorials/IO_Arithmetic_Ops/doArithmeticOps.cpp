83ac017b52eed7605e10f3da413d9621df132ee6
cs20180603 <random0411@kaist.ac.kr>
2020-05-14 23:03:40 -0700

    removed old files and binary files

--- Baekjoon-Solutions/Basic-Tutorials/IO_Arithmetic_Ops/doArithmeticOps.cpp
+++
@@ -1,9 +0,0 @@
-// https://www.acmicpc.net/problem/10869
-#include <iostream>
-using namespace std;
-
-int main() {
-    int a, b, c;
-    cin >> a >> b >> c;
-    cout << a + b << '\n' << a - b << '\n' << a * b << '\n' << a / b << '\n' << a % b << '\n';
-}
