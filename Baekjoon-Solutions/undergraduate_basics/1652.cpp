ba6384d4631a45d15fd82e0a7d3f611872ac109a
cs20180603 <random0411@kaist.ac.kr>
2020-08-07 09:40:58 -0700

    solved https://www.acmicpc.net/problem/1652

---
+++ Baekjoon-Solutions/undergraduate_basics/1652.cpp
@@ -0,0 +1,54 @@
+// https://www.acmicpc.net/problem/1652
+// find row/column that contains two adjacent . (not X)
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+
+int main() {
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    int n, place_row = 0, place_col = 0;
+    vector<string> row_vec;
+
+    cin >> n;
+    bool blocked;
+    for (int i = 0; i < n; i++) {
+        string row;
+        cin >> row;
+        row_vec.push_back(row);
+        
+        blocked = true;
+        char prev = 'X';
+        for (int j = 0; j < row.length(); j++) {
+            char cur = row.at(j);
+            if (!blocked && cur == 'X') blocked = true;
+            if (blocked && cur == '.' && prev == '.') {
+                place_row++;
+                blocked = false;
+            }
+            prev = cur;
+            
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        blocked = true;
+        char prev = 'X';
+        for (int j = 0; j < n; j++) {
+            string row = row_vec[j];
+            char cur = row[i];
+            if (!blocked && cur == 'X') blocked = true;
+            if (blocked && cur == '.' && prev == '.') {
+                place_col++;
+                blocked = false;
+            }
+            prev = cur;
+        }
+    }
+
+    cout << place_row << ' ' << place_col << '\n';
+}
