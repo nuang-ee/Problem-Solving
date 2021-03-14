694ddbd7905bcab6a82b64b735352df41efe2717
cs20180603 <random0411@kaist.ac.kr>
2021-03-13 21:01:31 -0800

    change naming convention

---
+++ Leetcode-Solutions/Hard/0004_MedianOfTwoSortedArrays.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        
+    }
+};
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+
+
+}
